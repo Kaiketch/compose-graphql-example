fffc6bc8cb1b12604e6ebe5880f04beca11073f0
Kaiketch <akaike@kumofune.jp>
2022-12-31 12:55:53 +0900

    add viewer

--- feature/home/src/main/java/com/example/home/HomeScreen.kt
+++ feature/home/src/main/java/com/example/home/HomeScreen.kt
@@ -2,11 +2,15 @@ package com.example.home
 
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.remember
 import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
 fun HomeScreen(
     homeViewModel: HomeViewModel = hiltViewModel()
 ) {
-    Text(text = homeViewModel.test)
+    val uiState = homeViewModel.uiState.collectAsState()
+
+    Text(text = uiState.value.viewer?.login ?: "empty")
 }
--- feature/home/src/main/java/com/example/home/HomeViewModel.kt
+++ feature/home/src/main/java/com/example/home/HomeViewModel.kt
@@ -1,10 +1,38 @@
 package com.example.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.apollographql.apollo3.ApolloClient
+import com.example.ViewerQuery
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor() : ViewModel() {
-    val test = "test"
+class HomeViewModel @Inject constructor(
+    private val apolloClient: ApolloClient
+) : ViewModel() {
+
+    data class HomeUiState(
+        val viewer: ViewerQuery.Viewer? = null,
+    )
+
+    private val _uiState = MutableStateFlow(HomeUiState())
+    val uiState: StateFlow<HomeUiState> = _uiState
+
+    init {
+        viewModelScope.launch {
+            runCatching {
+                apolloClient.query(ViewerQuery()).execute().data?.viewer ?: throw IllegalStateException()
+            }.onSuccess {
+                _uiState.value = _uiState.value.copy(
+                    viewer = it,
+                )
+            }.onFailure {
+
+            }
+        }
+    }
 }
