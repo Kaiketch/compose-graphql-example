8e95e858bce12af84fd96ed8343e9dcc771ded95
Kaiketch <akaike@kumofune.jp>
2023-01-06 18:49:35 +0900

    add ApolloMutationResult

--- data/graphql/src/main/java/com/example/graphql/ApolloResult.kt
+++ data/graphql/src/main/java/com/example/graphql/ApolloResult.kt
@@ -1,6 +1,7 @@
 package com.example.graphql
 
 import com.apollographql.apollo3.api.ApolloResponse
+import com.apollographql.apollo3.api.Mutation
 import com.apollographql.apollo3.api.Query
 
 data class ApolloResult<T : Query.Data>(
@@ -26,6 +27,36 @@ data class ApolloResult<T : Query.Data>(
 
         fun <T : Query.Data> error(): ApolloResult<T> {
             return ApolloResult(
+                data = null,
+                isLoading = false
+            )
+        }
+    }
+}
+
+data class ApolloMutationResult<T : Mutation.Data>(
+    val data: T?,
+    val isLoading: Boolean
+) {
+    companion object {
+        fun <T : Mutation.Data> startLoading(): ApolloMutationResult<T> {
+            return ApolloMutationResult(
+                data = null,
+                isLoading = true
+            )
+        }
+
+        fun <T : Mutation.Data> success(
+            response: ApolloResponse<T>
+        ): ApolloMutationResult<T> {
+            return ApolloMutationResult(
+                data = response.data,
+                isLoading = false
+            )
+        }
+
+        fun <T : Mutation.Data> error(): ApolloMutationResult<T> {
+            return ApolloMutationResult(
                 data = null,
                 isLoading = false
             )
